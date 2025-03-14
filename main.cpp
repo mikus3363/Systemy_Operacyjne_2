e59b8ae2629dbbdb05ec2b19108c166e832f9b9d
mikus3363 <273024@student.pwr.edu.pl>
2025-03-14 18:39:46 +0100

    end

--- main.cpp
+++ main.cpp
@@ -1,76 +1,131 @@
 #include <iostream>
 #include <pthread.h>
 #include <unistd.h>
+#include <atomic>
+
 using namespace std;
 
-class Synchro{
+class Fork{
 public:
-    pthread_mutex_t mutex;
-    bool locked;
+    atomic<bool> locked; // zastosowanie zmiennej atomowej aby uniknąć przerwania przez inne wątki, operacje są wykonywane w sposób niepodzielny
 
-    Synchro(){
-        pthread_mutex_init(&mutex, nullptr);
-        locked = false;
+    Fork() {
+        locked = false; // domyślnie widelec jest odblokowany
     }
 
-    void lock(){
-        while(locked){
-            usleep(1000);
-        }
-        locked = true;
-        pthread_mutex_lock(&mutex);
+    bool tryLock() {
+        return !locked.exchange(true); // próba zablokowania widelec; zwraca true, jeśli uda się zablokować (czyli widelec był odblokowany)
     }
 
-    void unlock(){
-        locked = false;
-        pthread_mutex_unlock(&mutex);
+    void unlock() {
+        locked = false; // odblokowanie widelec
     }
 };
 
-Synchro synchro;
+Fork* forks;
+
+struct PhilosopherData {
+    int id; // id filozofa
+    int numPhilosophers; // liczba filozofów
+    string state; // stan filozofa myśli/je
+};
 
 void* philosopher(void* arg) {
-    int id = *(int*)arg;
+    PhilosopherData* data = (PhilosopherData*)arg; // pobranie danych filozofa
+    int id = data->id; // identyfikator filozofa przypisanie
+    int numPhilosophers = data->numPhilosophers; // liczba filozofów przypisanie
 
     while (true) {
-        // Jedzenie
-        synchro.lock();
-        cout << "Filozof " << id << " je." << endl;
-        synchro.unlock();
+        data->state = "mysli     "; // ustawienie stanu filozofa
         usleep(1000000); // pauza na 1 sekundę
 
-        // Sen
-        synchro.lock();
-        cout << "Filozof " << id << " spi." << endl;
-        synchro.unlock();
+        // wzięcie widelców
+        if(id % 2 == 0){ // warunek jeżeli filozof ma parzysty numer
+            int rightForkId = id; // id widelca po prawej stronie
+            while (!forks[rightForkId].tryLock()) { // próba zablokowania widelca po prawej stronie filozofa
+                usleep(1000); // jeżeli widelec jest zajęty to krótka pauza
+            }
+            int leftForkId = (id + 1) % numPhilosophers; // id widelca po lewej stronie
+            while (!forks[leftForkId].tryLock()) { // próba zablokowania widelca po lewej stronie filozofa
+                usleep(1000); // jeżeli widelec jest zajęty to krótka pauza
+            }
+        } else{ // dla filozofów o nieparzystym numerze
+            int leftForkId = id; // id widelca po lewej stronie
+            while (!forks[leftForkId].tryLock()) { // próba zablokowania widelca po lewej stronie filozofa
+                usleep(1000); // jeżeli widelec jest zajęty to krótka pauza
+            }
+            int rightForkId = (id + 1) % numPhilosophers; // id widelca po prawej stronie
+            while (!forks[rightForkId].tryLock()) { // próba zablokowania widelca po prawej stronie filozofa
+                usleep(1000); // jeżeli widelec jest zajęty to krótka pauza
+            }
+        }
+        data->state = "je        "; // ustawienie stanu filozofa
+        usleep(1000000); // pauza na 1 sekundę
+
+        // odłożenie widelców
+        if(id % 2 == 0) { // jeżeli filozof ma parzysty numer
+            int leftForkId = (id + 1) % numPhilosophers; // id widelca po lewej stronie
+            forks[leftForkId].unlock(); // odblokowanie widelca po lewej stronie
+            forks[id].unlock(); // odblokowanie widelca po prawej stronie
+        } else{ // jeżeli filozof ma nieparzysty numer
+            int rightForkId = (id + 1) % numPhilosophers; // id widelca po prawej stronie
+            forks[rightForkId].unlock(); // odblokowanie widelca po prawej stronie
+            forks[id].unlock(); // odblokowanie widelca po lewej stronie
+        }
+    }
+
+    return nullptr; // nigdy nie jest zwracane, bo pętla nieskończona
+}
+
+void* updateDisplay(void* arg) {
+    // wątek do aktualizaji wyświetlania
+    PhilosopherData* data = (PhilosopherData*)arg; // dane o filozofach
+    int numPhilosophers = data[0].numPhilosophers; // liczba filozofów
+
+    while (true) {
+        for (int i = 0; i < numPhilosophers; ++i) {
+            if(i % numPhilosophers == 0 && i != 0){
+                cout<<endl; // co liczbę filozofów nowa linia
+            }
+            cout << "Filozof " << i << ": " << data[i].state; // wyświetlenie co robią filozofowie
+        }
+        cout << endl;
+        cout << endl;
         usleep(1000000); // pauza na 1 sekundę
     }
 
-    return nullptr;
+    return nullptr; // nigdy nie jest zwracane, bo pętla nieskończona
 }
 
 int main() {
-    int numPhilosophers = 5; // liczba filozofów
+    int numPhilosophers = 7; // liczba filozofów
+
+    forks = new Fork[numPhilosophers];
 
     // utworzenie wątków filozofów
     pthread_t* threads_philosopher = new pthread_t[numPhilosophers]; // tablica wątków
-    int* id_philosopher = new int[numPhilosophers]; // tablica id filozofów
+    PhilosopherData* data = new PhilosopherData[numPhilosophers]; // tablica danych filozofów
 
     for (int i = 0; i < numPhilosophers; ++i) {
-        id_philosopher[i] = i;
-        pthread_create(&threads_philosopher[i], nullptr, philosopher, &id_philosopher[i]); // (wstaźnik na wątek, atrybut domyślny, funkcja rozpoczynająca wątek, argument do funkcji)
+        data[i].id = i; // ustawienie id filozofa
+        data[i].numPhilosophers = numPhilosophers; // ustawienie liczby filozofów
+        pthread_create(&threads_philosopher[i], nullptr, philosopher, &data[i]); // (wstaźnik na wątek, atrybut domyślny, funkcja rozpoczynająca wątek, argument do funkcji)
     }
 
+    pthread_t updateThread; // wątek do aktualizacji wyświetlania
+    pthread_create(&updateThread, nullptr, updateDisplay, data); // utworzenie aktualnego wątku do wyświetlania. Konstrukcja tak jak wyżej
 
     // oczekiwanie na zakończenie wątków (nigdy nie nastąpi)
     for (int i = 0; i < numPhilosophers; ++i) {
         pthread_join(threads_philosopher[i], nullptr);
     }
 
+    pthread_join(updateThread, nullptr); // czekaj na zakończenie wątku update
+
     // usunięcie zaalokowanej pamięci (nigdy nie nastąpi)
     delete[] threads_philosopher;
-    delete[] id_philosopher;
+    delete[] data;
+    delete[] forks;
 
     return 0;
 }
-
